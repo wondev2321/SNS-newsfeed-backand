b2cd5f7d5a82fe7ca16c8875a0ac64372bc84920
wondev2321 <joowon2321@gmail.com>
2024-03-21 18:35:04 +0900

    개발 환경 구축

---
+++ src/main/java/com/example/snsnewsfeedbackand/SnsNewsfeedBackandApplication.java
@@ -0,0 +1,13 @@
+package com.example.snsnewsfeedbackand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsNewsfeedBackandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsNewsfeedBackandApplication.class, args);
+    }
+
+}
